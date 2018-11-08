1b04e2e7409252e9dfa91b137a8a1e8a407ada6a
sophia <sophiamuller@Sophias-MacBook-Pro.local>
2018-11-08 17:08:41 +0100

    Add first message

---
+++ docs/precache-manifest.ae96266f64c15fe7f852f23075303087.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0eb3ebdaa5f584baa0a6",
+    "url": "/static/js/main.0eb3ebda.chunk.js"
+  },
+  {
+    "revision": "57a8637701f48ef73559",
+    "url": "/static/js/1.57a86377.chunk.js"
+  },
+  {
+    "revision": "0eb3ebdaa5f584baa0a6",
+    "url": "/static/css/main.da03e078.chunk.css"
+  },
+  {
+    "revision": "9f9d4d7bf5081f3c8bb621b95a577010",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-class App extends Component {
+import './App.css'
+import Door from './Door.js'
+import Counter from './Counter.js'
+
+class App extends React.Component {
+  state = {
+    doors: this.getRandomDoors()
+  }
+
+  getRandomDoors() {
+    return [
+      { text: 'Foo' },
+      { text: 'Bar' },
+      { text: 'Baz' },
+      { text: 'Foobar' },
+      { text: 'Lorem' },
+      { text: 'ipsum' },
+      { text: 'dolor' },
+      { text: 'sit' },
+      { text: 'hello' },
+      { text: 'world' },
+      { text: 'Foo' },
+      { text: 'Bar' },
+      { text: 'Baz' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' },
+      { text: 'Foo' }
+    ]
+      .map((item, index, arr) => ({
+        ...item,
+        num: index + 1,
+        isOpen: false,
+        total: arr.length,
+        rand: Math.random()
+      }))
+      .sort((a, b) => a.rand - b.rand)
+      .map(item => {
+        delete item.rand
+        return item
+      })
+  }
+
+  renderDoors() {
+    return this.state.doors.map(door => (
+      <Door key={door.num} {...door} onClick={() => this.openDoor(door.num)} />
+    ))
+  }
+
+  openDoor = num => {
+    const { doors } = this.state
+    const index = doors.findIndex(d => d.num === num)
+    const newDoors = [
+      ...doors.slice(0, index),
+      { ...doors[index], isOpen: true },
+      ...doors.slice(index + 1)
+    ]
+
+    this.setState({
+      doors: newDoors
+    })
+  }
+
   render() {
+    const openDoorsNum = this.state.doors.filter(item => item.isOpen).length
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <section className="App">
+        <Counter num={openDoorsNum} />
+        {this.renderDoors()}
+      </section>
+    )
   }
 }
 
-export default App;
+export default App
